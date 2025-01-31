029bbdc9de3a07d775f92f6f0ce23ab642628cff
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-01-30 22:46:41 -0300

    Adicionando rotas de delete e patch

--- src/modules/express.ts
+++ src/modules/express.ts
@@ -1,4 +1,4 @@
-import e from "express";
+import e, { NextFunction, Request, Response } from "express";
 import UserModel from "../models/User.Modal";
 
 interface User {
@@ -8,25 +8,69 @@ interface User {
   password: string;
 }
 
+const PORT = 8080;
 const app = e();
 app.use(e.json());
-const PORT = 8080;
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body);
+  next();
+});
 
 app.get("/home", (req, res) => {
   res.contentType("text/html");
   res.status(200).send("<h1>Hello World!</h1>");
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
   try {
-    const users = UserModel.find();
+    const users = await UserModel.find();
     res.json(users);
   } catch (error) {
-    res.json({ error: error });
+    res.json({ error: (error as Error).message });
   }
 });
 
-app.post("/user", async (req, res) => {
+app.get("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findById(id);
+    res.json(user);
+  } catch (error) {
+    res.json({ message: (error as Error).message });
+  }
+});
+
+app.patch("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const { email }: User = req.body;
+    const user = await UserModel.findByIdAndUpdate(
+      id,
+      {
+        email: email,
+      },
+      { new: true }
+    );
+
+    res.json(user);
+  } catch (error) {
+    res.json({ message: (error as Error).message });
+  }
+});
+
+app.delete("/user/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.body.id;
+    const user = await UserModel.findByIdAndDelete(id);
+
+    res.json(user);
+  } catch (error) {
+    res.json({ message: (error as Error).message });
+  }
+});
+
+app.post("/users", async (req, res) => {
   try {
     const { firstName, lastName, email, password }: User = req.body;
     const user = await UserModel.create({
@@ -38,7 +82,7 @@ app.post("/user", async (req, res) => {
 
     res.json(user);
   } catch (error) {
-    res.json({ error: error });
+    res.json({ message: (error as Error).message });
   }
 });
 
